9c6a3a10f42ee255cf83811616a9e2d298dde47a
Bennett Hoffman <bhoffman@palantir.com>
2015-08-13 17:26:00 -0400

    Disable bottles

--- docker171.rb
+++ docker171.rb
@@ -5,12 +5,12 @@ class Docker171 < Formula
       :tag => "v1.7.1",
       :revision => "786b29d4db80a6175e72b47a794ee044918ba734"
 
-  bottle do
-    cellar :any
-    sha256 "f6f3a78dcd9e9154b1285ab1e74b3ba1ba33f7466195b50caadf0b55c8028e22" => :yosemite
-    sha256 "91c81898fc2b781c43e734812da9310bfd9b207892ee07df518395e2df115bb7" => :mavericks
-    sha256 "dccb74261f9f0f70abf82852e3ade21914a6edaaee47f373af46cd7783c6a832" => :mountain_lion
-  end
+  #bottle do
+    #cellar :any
+    #sha256 "f6f3a78dcd9e9154b1285ab1e74b3ba1ba33f7466195b50caadf0b55c8028e22" => :yosemite
+    #sha256 "91c81898fc2b781c43e734812da9310bfd9b207892ee07df518395e2df115bb7" => :mavericks
+    #sha256 "dccb74261f9f0f70abf82852e3ade21914a6edaaee47f373af46cd7783c6a832" => :mountain_lion
+  #end
 
   option "without-completions", "Disable bash/zsh completions"
 
